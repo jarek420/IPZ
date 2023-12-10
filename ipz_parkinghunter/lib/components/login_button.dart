bc6122f46cb5513919ff848d886917a768c86d61
Worysz <woryszlukasz@gmail.com>
2023-12-10 21:24:46 +0100

    Adding some styles to login button

--- ipz_parkinghunter/lib/components/login_button.dart
+++ ipz_parkinghunter/lib/components/login_button.dart
@@ -9,7 +9,13 @@ class LoginButton extends StatelessWidget
   {
     return Container
     (
-      child: Center(child: Text("Zaloguj")),
+      child: Center(child:Text("Zaloguj",
+      style: TextStyle(color: Color.fromARGB(247, 247, 247, 247))),
+      ),
+      decoration: BoxDecoration(color: Colors.black,
+      borderRadius: BorderRadius.circular(10.0)),
+      padding: EdgeInsets.all(25),
+      margin: EdgeInsets.symmetric(horizontal: 25),
     );
       
   }
