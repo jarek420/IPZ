bc8535babb9eeffaca979d7fc0f01ae76496cf7f
Wodnik3z <krawczak01@gmail.com>
2023-12-17 21:30:07 +0100

    commit

--- ipz_parkinghunter/lib/main.dart
+++ ipz_parkinghunter/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // Setting the LoginPage as a starting Page.
-      home: MainPage(),
+      home: LoginPage(),
     );
   }
 }
